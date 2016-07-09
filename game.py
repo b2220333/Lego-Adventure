9cba8649e03fee19c42013b2f8b082b5ef530f11
LIHAO <h_91@me.com>
2016-07-09 16:42:44 -0700

    Created initial state of the game scene

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
 # LIHAO LIN Created on 07/08/2016 All right reserved
+from direct.showbase.ShowBase import ShowBase
+
+
+class LegoAdventure(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+
+game = LegoAdventure()
+game.run()
