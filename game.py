1aec3cc2e597430e919f57b8cacbff4329ace0e7
LIHAO <h_91@me.com>
2016-07-09 16:46:16 -0700

    Setup lights for the scene

--- game.py
+++ game.py
@@ -5,6 +5,19 @@ from direct.showbase.ShowBase import ShowBase
 class LegoAdventure(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        self.setupLights()
+
+    def setupLights(self):
+        alight = AmbientLight('ambientLight')
+        alight.setColor(Vec4(0.5, 0.5, 0.5, 1))
+        alightNP = render.attachNewNode(alight)
+        dlight = DirectionalLight('directionalLight')
+        dlight.setDirection(Vec3(1, 1, -1))
+        dlight.setColor(Vec4(0.7, 0.7, 0.7, 1))
+        dlightNP = render.attachNewNode(dlight)
+        self.render.clearLight()
+        self.render.setLight(alightNP)
+        self.render.setLight(dlightNP)
 
 
 game = LegoAdventure()
