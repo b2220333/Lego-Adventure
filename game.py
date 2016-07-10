b0df688846f24dc28cc2b60f6b7c1529bfc7a33a
LIHAO <h_91@me.com>
2016-07-09 18:32:49 -0700

    Setup for control keys Setup for Physics World and debugNode

--- game.py
+++ game.py
@@ -1,12 +1,48 @@
 # LIHAO LIN Created on 07/08/2016 All right reserved
+import sys
 from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
+from direct.showbase.DirectObject import DirectObject
+from direct.showbase.InputStateGlobal import inputState
+from panda3d.core import AmbientLight, DirectionalLight
+from panda3d.core import Vec3, Vec4, Point3
+from panda3d.core import BitMask32
+from panda3d.core import NodePath, PandaNode
+from panda3d.bullet import BulletWorld
+from panda3d.bullet import BulletHelper
+from panda3d.bullet import BulletPlaneShape
+from panda3d.bullet import BulletBoxShape
+from panda3d.bullet import BulletRigidBodyNode
+from panda3d.bullet import BulletDebugNode
+from panda3d.bullet import BulletSphereShape
+from panda3d.bullet import BulletCapsuleShape
+from panda3d.bullet import BulletCharacterControllerNode
+from panda3d.bullet import BulletHeightfieldShape
+from panda3d.bullet import BulletTriangleMesh
+from panda3d.bullet import BulletTriangleMeshShape
+from panda3d.bullet import BulletSoftBodyNode
+from panda3d.bullet import BulletSoftBodyConfig
+from panda3d.bullet import ZUp
 
 
 class LegoAdventure(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.setupLights()
+        self.setupWorld()
 
+    def setupWorld(self):
+        self.debugNP = self.render.attachNewNode(BulletDebugNode('Debug'))
+        self.debugNP.show()
+        # created Bullet Physics World
+        self.world = BulletWorld()
+        self.world.setGravity(Vec3(0, 0, -9.81))
+        # add debugNode to the world
+        self.world.setDebugNode(self.debugNP.node())
+        # other setups
+        self.setupLights()
+        self.setupControlKeys()
+
+    # Setup functions for Game
     def setupLights(self):
         alight = AmbientLight('ambientLight')
         alight.setColor(Vec4(0.5, 0.5, 0.5, 1))
@@ -19,6 +55,25 @@ class LegoAdventure(ShowBase):
         self.render.setLight(alightNP)
         self.render.setLight(dlightNP)
 
+    def setupControlKeys(self):
+        self.accept('escape', self.Exit)
+        # self.accept('r', self.Reset)
+        self.accept('t', self.Debug)
+        inputState.watchWithModifiers('forward', 'w')
+        inputState.watchWithModifiers('reverse', 's')
+        inputState.watchWithModifiers('turnLeft', 'a')
+        inputState.watchWithModifiers('turnRight', 'd')
+        inputState.watchWithModifiers('jump', 'space')
+
+    def Exit(self):
+        # self.cleanup()
+        sys.exit(1)
+
+    def Debug(self):
+        if self.debugNP.isHidden():
+            self.debugNP.show()
+        else:
+            self.debugNP.hide()
 
 game = LegoAdventure()
 game.run()
